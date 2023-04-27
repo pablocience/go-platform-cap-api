0da5afb221c722a5c504a0c6f1d41a79186a9f80
Pablo Zarate <pablo.zarate@cience.com>
2023-04-27 10:51:28 -0600

    change apikey

--- src/middlewares/validateApiKey.middleware.ts
+++ src/middlewares/validateApiKey.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-const apiKeys = process.env.GO_API_KEY;
+const apiKeys = process.env.GO_CAP_API_KEY;
 export const checkXApiKey = (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.headers['x-api-key'];
   const customer_id = req.headers['customer-id'];
