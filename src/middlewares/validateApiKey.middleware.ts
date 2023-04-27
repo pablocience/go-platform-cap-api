3284fd83166700704a093c8d5589f138f6c3ecbb
Pablo Zarate <pablo.zarate@cience.com>
2023-04-27 13:18:37 -0600

    added code

--- src/middlewares/validateApiKey.middleware.ts
+++ src/middlewares/validateApiKey.middleware.ts
@@ -1,15 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-const apiKeys = process.env.GO_CAP_API_KEY;
+
 export const checkXApiKey = (req: Request, res: Response, next: NextFunction) => {
-  const apiKey = req.headers['x-api-key'];
+  const apiKey = req.headers['x-looker-key'];
+  const apiKeys = process.env.GO_CAP_API_KEY;
   const customer_id = req.headers['customer-id'];
   if (apiKey !== apiKeys || !apiKey || !apiKeys) {
+    let code = '401UAI-'
+    if(apiKey !== apiKeys) code += 'DF'
+    if(!apiKey) code += 'NAK'
+    if(!apiKeys) code += 'NENVAK'
     return res.status(401).json({
       status: 401,
       message: 'Unauthorized',
-      error:'401UAI'
+      error:code
     });
   }
   if (!customer_id || customer_id === '') {
